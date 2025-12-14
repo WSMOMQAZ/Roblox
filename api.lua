5bff10cf1220ff5c99b9d7cf0339887194f01c7d
WSMOMQAZ <wsomoqaz@gmail.com>
2025-12-14 22:24:45 +0800

    创建 api.lua

---
+++ api.lua
@@ -0,0 +1,9 @@
+return {
+    enabled = true,              -- 总开关（false = 全部失效）
+    version = "1.0.0",
+
+    blacklist = {                -- UserId 黑名单
+        123456789,
+        987654321
+    }
+}
